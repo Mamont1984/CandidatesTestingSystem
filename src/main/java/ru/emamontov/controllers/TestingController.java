6dd2c1d4efbc4e572620d8f6cbd6ebd4c53ed52e
Evgeny Mamontov <mamontovevgeny@mail.ru>
2019-10-06 23:37:11 +0300

    TestingController improvements

--- src/main/java/ru/emamontov/controllers/TestingController.java
+++ src/main/java/ru/emamontov/controllers/TestingController.java
@@ -1,5 +1,6 @@
 package ru.emamontov.controllers;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,17 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tests")
 public class TestingController {
     @RequestMapping(value = "/{testId}", method = RequestMethod.POST)
-    public String startTestCase(@PathVariable int TestCaseId){
+    public String startTest(@PathVariable int testId, Model model){
         return null;
     }
 
     @RequestMapping(value = "/{testId}", method = RequestMethod.GET)
-    public String getCurrentQuestion(@PathVariable int TestCaseId){
+    public String getQuestion(@PathVariable int testId, Model model){
         return null;
     }
 
-    @RequestMapping(value = "/{testId}/{AnswerNumber}", method = RequestMethod.POST)
-    public String answerCurrentQuestion(@PathVariable int TestCaseId, int AnswerNumber){
+    @RequestMapping(value = "/{testId}/{questionId}", method = RequestMethod.POST)
+    public String answerQuestion(@PathVariable int testId, int questionId, Model model){
         return null;
     }
 }
