d89f1b8169486d712534a634a055d0c1f8ff132d
Evgeny Mamontov <mamontovevgeny@mail.ru>
2019-10-10 23:58:07 +0300

    Entities changes

--- src/main/java/ru/emamontov/entities/Test.java
+++ src/main/java/ru/emamontov/entities/Test.java
@@ -3,8 +3,29 @@ package ru.emamontov.entities;
 import java.util.List;
 
 public class Test {
-    private String testCaseName;
+    private String name;
     private User author;
     private List<Question> questions;
     private int minimumPassingResult;
+
+    public Test(String name, User author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
 }
--- src/main/java/ru/emamontov/entities/User.java
+++ src/main/java/ru/emamontov/entities/User.java
@@ -3,6 +3,10 @@ package ru.emamontov.entities;
 public class User {
     private String name;
 
+    public User(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
