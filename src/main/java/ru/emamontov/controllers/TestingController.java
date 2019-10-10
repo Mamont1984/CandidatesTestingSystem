6f7499b2cbf295c29473ad1b3a3bb60ba48b6f14
Evgeny Mamontov <mamontovevgeny@mail.ru>
2019-10-11 00:07:51 +0300

    TestingController -Add getTestsList method

--- src/main/java/ru/emamontov/controllers/TestingController.java
+++ src/main/java/ru/emamontov/controllers/TestingController.java
@@ -1,21 +1,31 @@
 package ru.emamontov.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.emamontov.services.TestingService;
 
-@RestController
+@Controller
 @RequestMapping("/tests")
 public class TestingController {
+    @Autowired
+    TestingService testingService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getTestsList(Model model){
+
+        model.addAttribute("selections", testingService.getTestsList());
+        return "tests/testsList";
+    }
+
     @RequestMapping(value = "/{testId}", method = RequestMethod.POST)
     public String startTest(@PathVariable int testId, Model model){
         return null;
     }
 
-    @RequestMapping(value = "/{testId}", method = RequestMethod.GET)
-    public String getQuestion(@PathVariable int testId, Model model){
+    @RequestMapping(value = "/{testId}/{questionId}", method = RequestMethod.GET)
+    public String getQuestion(@PathVariable int testId, int questionId, Model model){
         return null;
     }
 
